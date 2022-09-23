2af2c01630aae107b685a5ae692bc5b3474e124f
babukharat <kharatbabu55@gmail.com>
2022-09-23 21:19:32 +0530

    Added pagination

--- new_crud/src/AppRoutes/Inventory.js
+++ new_crud/src/AppRoutes/Inventory.js
@@ -1,79 +1,45 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button';
+import React from "react";
+import "../../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../../node_modules/react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
+import BootstrapTable from "react-bootstrap-table-next";
+import paginationFactory from "react-bootstrap-table2-paginator";
 
+export const productsGenerator = (quantity) => {
+  const items = [];
+  for (let i = 0; i < quantity; i++) {
+    items.push({ id: i, name: `Item name ${i}`, price: 2100 + i });
+  }
+  return items;
+};
+
+const products = productsGenerator(100);
+
+const columns = [
+  {
+    dataField: "id",
+    text: "Product ID",
+    sort: true,
+  },
+  {
+    dataField: "name",
+    text: "Product Name",
+    sort: true,
+  },
+  {
+    dataField: "price",
+    text: "Product Price",
+  },
+];
 export default function Inventory() {
   return (
-  
-  <div>
-    <br></br>
-    <form class="form-inline">
-      <input class="form-control mr-sm-2" type="search" placeholder="Product ID/ Product Name" aria-label="Search"/>
-      <br></br>
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-    <br></br>
-    <h1 className='text-primary'>All Inventory Products</h1>
-
-    <table class="table table-bordered">
-  <thead>
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">Product Name</th>
-      <th scope="col">Product Image</th>
-      <th scope="col">Product Category</th>
-      <th scope="col">Product Price</th>
-      <th scope="col">Purchased from Vendor </th>
-      <th scope="col">Product Stock</th>
-      <th scope="col">Units Available</th>
-      <th scope="col">Reorder Level</th>
-      <th scope="col">GST for Product</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>@fat</td>
-      <td>@fat</td>
-      <td>@fat</td>
-      <td>@fat</td>
-      <td>@fat</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td >Larry the Bird</td>
-      <td>@twitter</td>
-      <td>@twitter</td>
-      <td>@twitter</td>
-      <td>@twitter</td>
-      <td>@twitter</td>
-      <td>@twitter</td>
-      <td>@twitter</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-        <div className="d-grid gap-2">
-        <Button variant="primary" size="lg">
-                Print 
-              </Button>
-        </div>
-
-  </div>
-  )
+    <div className="App">
+      <BootstrapTable
+        bootstrap4
+        keyField="id"
+        data={products}
+        columns={columns}
+        pagination={paginationFactory({ sizePerPage: 5 })}
+      />
+    </div>
+  );
 }
--- new_crud/src/index.js
+++ new_crud/src/index.js
@@ -1,21 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.js';
-import App from './AppRoutes/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.js";
+import App from "./AppRoutes/App";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <div className='container bg-light'>
+    <div className="container-fluid bg-light">
       <BrowserRouter>
-        <App/>
+        <App />
       </BrowserRouter>
-      </div>
-  
-  
+    </div>
   </>
-
 );
