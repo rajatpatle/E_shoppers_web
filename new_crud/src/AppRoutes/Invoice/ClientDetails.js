cca124cec104f2381106f87378c45d4125f8cb38
pythondjango117 <pythondjango117@gmail.com>
2022-10-23 09:37:55 +0530

    Revert "commited some changes in clientdetails"
    This reverts commit 51b9eb2cfc1f3ed58399e911e0e4a75a69a36ddb.

--- new_crud/src/AppRoutes/Invoice/ClientDetails.js
+++ new_crud/src/AppRoutes/Invoice/ClientDetails.js
@@ -1,7 +1,6 @@
 export default function ClientDetails({ clientId, clientAddress }) {
   return (
     <>
-    <h1>Git Practice creating changes</h1>
       <section className="mt-10">
         <h2>{clientId}</h2>
         <p>{clientAddress}</p>
