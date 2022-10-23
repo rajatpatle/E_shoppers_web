e9a301cca3cc1048e1a0452526bd946c6615202a
RajatBokde <bokderajat4@gmail.com>
2022-10-23 12:12:41 +0530

    commit some change

--- new_crud/src/AppRoutes/Invoice/ClientDetails.js
+++ new_crud/src/AppRoutes/Invoice/ClientDetails.js
@@ -1,6 +1,7 @@
 export default function ClientDetails({ clientId, clientAddress }) {
   return (
     <>
+ <h1>Git ...This is New .....line 1</h1>
     <h3>This is line 1</h3>
       <section className="mt-10">
         <h2>{clientId}</h2>
