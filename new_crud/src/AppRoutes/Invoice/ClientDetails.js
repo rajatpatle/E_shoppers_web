93802ff41ac23ccc42430127d5848930a332a3ae
Rajat Patle <rajats.patle@gmail.com>
2022-10-23 09:55:57 +0530

    rajat commit

--- new_crud/src/AppRoutes/Invoice/ClientDetails.js
+++ new_crud/src/AppRoutes/Invoice/ClientDetails.js
@@ -1,10 +1,12 @@
 export default function ClientDetails({ clientId, clientAddress }) {
   return (
     <>
+    <h3>This is line 1</h3>
       <section className="mt-10">
         <h2>{clientId}</h2>
         <p>{clientAddress}</p>
       </section>
+      <h2>This is line 2</h2>
     </>
   );
 }
