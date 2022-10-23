256a8af9389575b76ebe5ca4bd66da80566069c2
pythondjango117 <pythondjango117@gmail.com>
2022-10-23 09:59:19 +0530

    commited some changes in clientdetails

--- new_crud/src/AppRoutes/Invoice/ClientDetails.js
+++ new_crud/src/AppRoutes/Invoice/ClientDetails.js
@@ -2,8 +2,10 @@ export default function ClientDetails({ clientId, clientAddress }) {
   return (
     <>
       <section className="mt-10">
+        
         <h2>{clientId}</h2>
         <p>{clientAddress}</p>
+        <h3>Git new practice</h3>
       </section>
     </>
   );
